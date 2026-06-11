89e2834492bf7d944fe435a6a3e83cec8d4306c5
Asmi11404 <308266@ust.com>
2026-06-11 20:45:06 +0530

    Edit Profile Updatedd

--- Backend/src/routes/patientAuthRoutes.js
+++ Backend/src/routes/patientAuthRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 
 const {
@@ -7,9 +7,23 @@ const {
     loginPatient
 } = require("../controllers/patientAuthController");
 
+const {
+    getPatientById,
+    updatePatient
+} = require("../controllers/patientController");
 router.post("/register", registerPatient);
 
 router.post("/login", loginPatient);
 
-
+//react-native
+router.put(
+    "/patient-profile/:uhid",
+    authMiddleware,
+    updatePatient
+);
+router.get(
+    "/patient-profile/:uhid",
+    authMiddleware,
+    getPatientById
+);
 module.exports = router;
